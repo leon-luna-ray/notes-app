66e3893fb46e426380d144a7c684609ee3a95c2c
Ray Luna <leon.luna.ray@gmail.com>
2021-02-09 17:52:53 -0800

    got get route to work

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -116,7 +116,9 @@ const handleRenderSaveBtn = () => {
 
 // Render the list of note titles
 const renderNoteList = async (notes) => {
+  console.log(notes);
   let jsonNotes = await notes.json();
+  console.log(jsonNotes)
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
--- server.js
+++ server.js
@@ -4,20 +4,27 @@ const fs = require('fs');
 const moment = require('moment');
 const path = require('path');
 const uuid = require('uuid');
+const noteDb = fs.readFileSync('./db/db.json');
+const notes = JSON.parse(noteDb);
+let db = require('./db/db.json');
 
-const notesDb = fs.readFileSync('./db/db.json');
-let notes = JSON.stringify(notesDb);
-
+console.log(noteDb);
 console.log(notes);
 
 // Sets up the Express App
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+
+
+// let notes = notesDb.json();
+
+
 
 // HTML routes to get the pages to serve
 app.get('/', (req, res) => {
@@ -30,44 +37,51 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// Get route in case they don't navigate to any route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-//API routes to get data
+//API routes to get data 
 app.get('/api/notes', (req, res) => {
-  // read the `db.json` file and return all saved notes as JSON. Not sure how to test this
+  return res.json(notes);
 }); // get
 
 //Receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. Added id and timestamp.
 
-app.post('/api/notes', (req, res) => {
-  const newNote = {
-    id: uuid.v4(),
-    time: moment().format(),
-    title: req.body.title,
-    text: req.body.text,
-  };
+// app.post('/api/notes', (req, res) => {
+//   console.log(req)
+//   res.send(req)`
+// }); // post route
 
-  // Need to find out how to append
-  fs.writeFileSync('./db/db', JSON.stringify(newNote));
-  res.end();
-  console.log(newNote);
-  }); // post
+// app.post('/api/notes', (req, res) => {
+//   const newNote = {
+//     id: uuid.v4(),
+//     time: moment().format(),
+//     title: req.body.title,
+//     text: req.body.text,
+//   };
 
-  // Update note route
-  app.put('/api/notes', (req, res) => {
-    const content = JSON.parse(file_content);
-    const updatedNote = content[req.id];
-    updatedNote.push(req);
+//   // Need to find out how to append currently not working
+//   fs.writeFile(noteDb, newNote);
 
-  }); // update
+//   res.send(newNote);v
+  
+//   console.log(newNote);
+//   }); // post
 
-  // delete note route (bonus)
-  app.delete('/api/notes', (req, res) => {
+  // // Update note route
+  // app.put('/api/notes', (req, res) => {
+  //   const content = JSON.parse(file_content);
+  //   const updatedNote = content[req.id];
+  //   updatedNote.push(req);
 
-  }); // delete
+  // }); // update
+
+  // // delete note route (bonus)
+  // app.delete('/api/notes', (req, res) => {
+
+  // }); // delete
+
+  // Get route in case they don't navigate to any route
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/index.html'));
+// }); // *
 
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
