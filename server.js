1ff6021db8a3373060182f7b43d0099b7924143c
Ray Luna <leon.luna.ray@gmail.com>
2021-02-13 16:55:20 -0800

    got the note to send and save on user click

--- server.js
+++ server.js
@@ -50,7 +50,18 @@ app.post('/api/notes', (req, res) => {
         console.log(err)
     } 
   });
+
+  res.send(newNote);
 }); // post route
+
+app.delete('/api/notes/:id', (req, res) => {
+    console.log('delete request recived!')
+});
+
+// * `DELETE /api/notes/:id` should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+
+
 
   // // Update note route
   // app.put('/api/notes', (req, res) => {
